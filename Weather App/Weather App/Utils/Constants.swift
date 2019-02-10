70528cd0d8ebb1270700db08b72895eaceb265ed
Renê Xavier <renefx@gmail.com>
2019-02-10 12:27:55 -0200

    Open Weather URL and key

---
+++ Weather App/Weather App/Utils/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Weather App
+//
+//  Created by Renê Xavier on 10/02/19.
+//  Copyright © 2019 STRV. All rights reserved.
+//
+
+import Foundation
+
+struct Titles {
+    static let APP_NAME = "Weather App"
+}
+
+struct OpenWeatherAPI {
+    static let url = "api.openweathermap.org/data/2.5/forecast?"
+    static let apiKey = "b496cbb102e3b82b4e47438315bd449f"
+}
+
